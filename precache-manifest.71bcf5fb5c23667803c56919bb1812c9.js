f1f89fb5b16ee3745c139b2d222d21746b3c8729
Slava Polishchuk <dizerllc@gmail.com>
2020-07-07 22:18:46 +0300

    Updates

---
+++ precache-manifest.71bcf5fb5c23667803c56919bb1812c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f0969a315b6a6f5ebc1a798fb256c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80bbdc06ec9158edae3a",
+    "url": "./static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "c647a2180534dc9b81db",
+    "url": "./static/css/main.e81c6f72.chunk.css"
+  },
+  {
+    "revision": "80bbdc06ec9158edae3a",
+    "url": "./static/js/2.a29cd344.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a29cd344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c647a2180534dc9b81db",
+    "url": "./static/js/main.b9db9462.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.71bcf5fb5c23667803c56919bb1812c9.js"
 );
 
 self.addEventListener('message', (event) => {
